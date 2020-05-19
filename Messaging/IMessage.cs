85f5157c126e2b56bfa65299b1fa7cfb32cc1ba2
nSafari <nsafarinia@it-street.cologne>
2020-05-20 02:49:05 +0430

    add IMessage.cs

---
+++ Messaging/IMessage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Messaging
+{
+    /// <summary>
+    /// Representation of a Message, containing a Payload and MetaData. Typical examples of Messages are Commands and
+    /// Events.
+    /// <para></para>
+    /// Instead of implementing {@code Message} directly, consider implementing
+    /// <see cref="org.axonframework.commandhandling.CommandMessage"/> or <see cref="EventMessage"/> instead.
+    /// <typeparam name="T">The type of payload contained in this Message</typeparam>
+    /// @author nSafari
+    /// @since 2.0
+    /// </summary>
+    public interface IMessage<T>
+    {
+        /// <summary>
+        /// Returns the identifier of this message. Two messages with the same identifiers should be interpreted as
+        /// different representations of the same conceptual message. In such case, the meta-data may be different for both
+        /// representations. The payload <em>may</em> be identical.
+        /// @return the unique identifier of this message
+        /// </summary>
+        String GetIdentifier();
+
+        /// <summary>
+        /// Returns the meta data for this event. This meta data is a collection of key-value pairs, where the key is a
+        /// String, and the value is a serializable object.
+        /// @return the meta data for this event
+        /// </summary>
+        MetaData GetMetaData();
+
+        /// <summary>
+        /// Returns the payload of this Event. The payload is the application-specific information.
+        /// @return the payload of this Event
+        /// </summary>
+        T GetPayload();
+
+        /// <summary>
+        /// Returns the type of the payload.
+        /// <para></para>
+        /// Is semantically equal to {@code getPayload().getClass()}, but allows implementations to optimize by using
+        /// lazy loading or deserialization.
+        /// @return the type of payload.
+        /// </summary>
+        Type GetPayloadType();
+
+        /// <summary>
+        /// Returns a copy of this Message with the given {@code metaData}. The payload remains unchanged.
+        /// <para></para>
+        /// While the implementation returned may be different than the implementation of {@code this}, implementations
+        /// must take special care in returning the same type of Message (e.g. EventMessage, DomainEventMessage) to prevent
+        /// errors further downstream.
+        /// @param metaData The new MetaData for the Message
+        /// @return a copy of this message with the given MetaData
+        /// </summary>
+        IMessage<T> WithMetaData<V>(Dictionary<String, V> metaData);
+    }
+}
