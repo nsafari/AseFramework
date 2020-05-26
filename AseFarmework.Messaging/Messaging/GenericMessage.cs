fa4a940529a615456c4f966209deeaef919119e9
nSafari <nassersafari@gmail.com>
2020-05-27 03:27:00 +0430

    Add GenericMessage

---
+++ AseFarmework.Messaging/Messaging/GenericMessage.cs
@@ -0,0 +1,24 @@
+﻿namespace Ase.Messaging.Messaging
+{
+    public class GenericMessage<T>: AbstractMessage<T>
+    {
+        public GenericMessage(string identifier) : base(identifier)
+        {
+        }
+
+        public override MetaData GetMetaData()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override T GetPayload()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IMessage<T> WithMetaData(MetaData metaData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AseFarmework.Messaging/Messaging/UnitOfWork/CurrentUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ase.Messaging.Messaging.UnitOfWork
+{
+    public class CurrentUnitOfWork
+    {
+
+        /// <summary>
+        /// Returns the Correlation Data attached to the current Unit of Work, or an empty {@link MetaData} instance
+        /// if no Unit of Work is started.
+        /// </summary>
+        /// <returns>a MetaData instance representing the current Unit of Work's correlation data, or an empty MetaData
+        /// instance if no Unit of Work is started.</returns>
+        /// <see cref="UnitOfWork.getCorrelationData()"/>
+        /// <exception cref="NotImplementedException"></exception>
+        public static MetaData CorrelationData()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
