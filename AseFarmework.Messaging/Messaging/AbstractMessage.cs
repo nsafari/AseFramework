3e0c2b562586240f74adc555cdb840f5e0de5d84
nSafari <nassersafari@gmail.com>
2020-05-26 05:32:37 +0430

    Add AbstractMessage.cs

---
+++ AseFarmework.Messaging/Messaging/AbstractMessage.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ase.Messaging.Messaging
+{
+    /// <summary>
+    /// Abstract base class for Messages.
+    /// </summary>
+    public abstract class AbstractMessage<T>: IMessage<T>
+    {
+        private readonly string _identifier;
+
+        /// <summary>
+        /// Initializes a new message with given identifier.
+        /// </summary>
+        /// <param name="identifier">the message identifier</param>
+        public AbstractMessage(string identifier)
+        {
+            _identifier = identifier;
+        }
+        
+        public string GetIdentifier()
+        {
+            return this._identifier;
+        }
+
+        public abstract MetaData GetMetaData();
+
+        public abstract T GetPayload();
+
+        public Type GetPayloadType()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMessage<T> AndMetaData(IReadOnlyDictionary<string, object> metaData)
+        {
+            if (metaData?.Count == 0)
+            {
+                return this;
+            }
+            return WithMetaData(GetMetaData().MergedWith(metaData));
+        }
+
+        public IMessage<T> WithMetaData(IReadOnlyDictionary<string, object> metaData)
+        {
+            if (GetMetaData().Equals(metaData))
+            {
+                return this;
+            }
+
+            return WithMetaData(MetaData.From(metaData));
+        }
+        
+        /// <summary>
+        /// Returns a new message instance with the same payload and properties as this message but given <code>metaData</code>.
+        /// </summary>
+        /// <param name="metaData">The metadata in the new message</param>
+        /// <returns>a copy of this instance with given metadata</returns>
+        protected abstract IMessage<T> WithMetaData(MetaData metaData);
+    }
+}
--- AseFarmework.Messaging/Messaging/IMessage.cs
+++ AseFarmework.Messaging/Messaging/IMessage.cs
@@ -7,7 +7,7 @@ namespace Ase.Messaging.Messaging
     /// Representation of a Message, containing a Payload and MetaData. Typical examples of Messages are Commands and
     /// Events.
     /// <para></para>
-    /// Instead of implementing {@code Message} directly, consider implementing
+    /// Instead of implementing <code>Message</code> directly, consider implementing
     /// <see cref="org.axonframework.commandhandling.CommandMessage"/> or <see cref="EventMessage"/> instead.
     /// <typeparam name="T">The type of payload contained in this Message</typeparam>
     /// @author nSafari
@@ -39,21 +39,31 @@ namespace Ase.Messaging.Messaging
         /// <summary>
         /// Returns the type of the payload.
         /// <para></para>
-        /// Is semantically equal to {@code getPayload().getClass()}, but allows implementations to optimize by using
+        /// Is semantically equal to <code>getPayload().getClass()</code>, but allows implementations to optimize by using
         /// lazy loading or deserialization.
         /// @return the type of payload.
         /// </summary>
         Type GetPayloadType();
 
         /// <summary>
-        /// Returns a copy of this Message with the given {@code metaData}. The payload remains unchanged.
+        /// Returns a copy of this Message with the given <code>metaData</code>. The payload remains unchanged.
         /// <para></para>
-        /// While the implementation returned may be different than the implementation of {@code this}, implementations
+        /// While the implementation returned may be different than the implementation of <code>this</code>, implementations
         /// must take special care in returning the same type of Message (e.g. EventMessage, DomainEventMessage) to prevent
         /// errors further downstream.
         /// @param metaData The new MetaData for the Message
         /// @return a copy of this message with the given MetaData
         /// </summary>
-        IMessage<T> WithMetaData<V>(Dictionary<String, V> metaData);
+        IMessage<T> WithMetaData(IReadOnlyDictionary<string, object> metaData);
+        
+        
+        /// <summary>
+        /// Returns a copy of this Message with it MetaData merged with the given <code>metaData</code>. The payload
+        /// remains unchanged.
+        /// </summary>
+        /// <param name="metaData">The MetaData to merge with</param>
+        /// <returns>a copy of this message with the given MetaData</returns>
+        IMessage<T> AndMetaData(IReadOnlyDictionary<string, object> metaData);
+
     }
 }
