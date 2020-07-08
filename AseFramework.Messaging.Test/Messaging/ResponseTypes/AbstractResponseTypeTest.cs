6bc348290ab12d325817047f3d43ecd31b1a22ae
nSafari <nassersafari@gmail.com>
2020-07-08 04:58:25 +0430

    Added AseFramework.Messaging.Test project

---
+++ AseFramework.Messaging.Test/Messaging/ResponseTypes/AbstractResponseTypeTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Ase.Messaging.Test.Messaging.ResponseTypes
+{
+    /// <summary>
+    /// Helper test implementation of {@link ResponseType} tests.
+    ///
+    /// <typeparam name="R">a generic for the expected response type of the {@link ResponseType} test subject</typeparam>
+    /// </summary>
+    public class AbstractResponseTypeTest<R>
+    {
+        
+    }
+}
