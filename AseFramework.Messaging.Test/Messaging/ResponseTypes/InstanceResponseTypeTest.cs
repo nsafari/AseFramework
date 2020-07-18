2b41fd872832e7c016f30109d25ab002ab8b3e11
nSafari <nassersafari@gmail.com>
2020-07-19 00:02:45 +0430

    Added all tests

--- AseFramework.Messaging.Test/Messaging/ResponseTypes/InstanceResponseTypeTest.cs
+++ AseFramework.Messaging.Test/Messaging/ResponseTypes/InstanceResponseTypeTest.cs
@@ -37,8 +37,210 @@ namespace Ase.Messaging.Test.Messaging.ResponseTypes
         {
             TestMatches("SomeUnboundedGenericQuery", DoesNotMatch);
         }
-        
-        
 
+        [Fact]
+        void TestMatchesReturnsTrueIfResponseTypeIsBoundedGenericOfProvidedType()
+        {
+            TestMatches("SomeBoundedGenericQuery", Matches);
+        }
+
+        [Fact]
+        void TestMatchesReturnsTrueIfResponseTypeIsMultiBoundedGenericOfProvidedType()
+        {
+            TestMatches("SomeMultiBoundedGenericQuery", Matches);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsGenericOfOtherType()
+        {
+            TestMatches("SomeNonMatchingBoundedGenericQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsArrayOfProvidedType()
+        {
+            TestMatches("SomeArrayQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsArrayWithSubTypeOfProvidedType()
+        {
+            TestMatches("SomeSubTypedArrayQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsArrayWithSuperTypeOfProvidedType()
+        {
+            TestMatches("SomeSuperTypedArrayQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsUnboundedGenericArray()
+        {
+            TestMatches("SomeUnboundedGenericArrayQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsBoundedGenericArrayOfProvidedType()
+        {
+            TestMatches("SomeBoundedGenericArrayQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsMultiBoundedGenericArrayOfProvidedType()
+        {
+            TestMatches("SomeMultiBoundedGenericArrayQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsGenericArrayOfOtherType()
+        {
+            TestMatches("SomeNonMatchingBoundedGenericArrayQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsListOfProvidedType()
+        {
+            TestMatches("SomeListQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsSubListOfProvidedType()
+        {
+            TestMatches("SomeSubListQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsSuperListOfProvidedType()
+        {
+            TestMatches("SomeSuperListQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsBoundedGenericListOfProvidedType()
+        {
+            TestMatches("SomeBoundedGenericListQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsUnboundedGenericList()
+        {
+            TestMatches("SomeUnboundedGenericListQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsMultiBoundedGenericListOfProvidedType()
+        {
+            TestMatches("SomeMultiBoundedGenericListQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsGenericListOfOtherType()
+        {
+            TestMatches("SomeNonMatchingBoundedGenericListQuery", DoesNotMatch);
+        }
+
+        // [Fact]
+        // void TestMatchesReturnsFalseIfResponseTypeIsUnboundedWildcardList()
+        // {
+        //     TestMatches("SomeUnboundedWildcardListQuery", DoesNotMatch);
+        // }
+
+        // [Fact]
+        // void TestMatchesReturnsFalseIfResponseTypeIsLowerBoundedWildcardList()
+        // {
+        //     TestMatches("SomeLowerBoundedWildcardListQuery", DoesNotMatch);
+        // }
+
+        // [Fact]
+        // void TestMatchesReturnsFalseIfResponseTypeIsUpperBoundedWildcardListOfProvidedType()
+        // {
+        //     TestMatches("SomeUpperBoundedWildcardListQuery", DoesNotMatch);
+        // }
+
+        // [Fact]
+        // void TestMatchesReturnsFalseIfResponseTypeIsWildcardListOfOtherType()
+        // {
+        //     TestMatches("SomeNonMatchingUpperBoundedWildcardQuery", DoesNotMatch);
+        // }
+
+        // [Fact]
+        // void TestMatchesReturnsFalseIfResponseTypeIsUnboundedGenericUpperBoundedWildcardList()
+        // {
+        //     TestMatches("SomeUnboundedGenericUpperBoundedWildcardListQuery", DoesNotMatch);
+        // }
+
+        // [Fact]
+        // void TestMatchesReturnsFalseIfResponseTypeIsGenericUpperBoundedWildcardListOfProvidedType()
+        // {
+        //     TestMatches("SomeGenericUpperBoundedWildcardListQuery", DoesNotMatch);
+        // }
+
+        // [Fact]
+        // void TestMatchesReturnsFalseIfResponseTypeIsMultiGenericUpperBoundedWildcardListOfProvidedType()
+        // {
+        //     TestMatches("SomeMultiGenericUpperBoundedWildcardListQuery", DoesNotMatch);
+        // }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsUnboundedListImplementationOfProvidedType()
+        {
+            TestMatches("SomeUnboundedListImplementationQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsBoundedListImplementationOfProvidedType()
+        {
+            TestMatches("SomeBoundedListImplementationQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsMultiUnboundedListImplementationOfProvidedType()
+        {
+            TestMatches("SomeMultiUnboundedListImplementationQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsMultiBoundedListImplementationOfProvidedType()
+        {
+            TestMatches("SomeMultiBoundedListImplementationQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsSetOfProvidedType()
+        {
+            TestMatches("SomeSetQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsStreamOfProvidedType()
+        {
+            TestMatches("SomeStreamQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsMapOfProvidedType()
+        {
+            TestMatches("SomeMapQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsTrueIfResponseTypeIsFutureOfProvidedType()
+        {
+            TestMatches("SomeFutureQuery", Matches);
+        }
+
+        [Fact]
+        void TestMatchesReturnsFalseIfResponseTypeIsListOfFutureOfProvidedType()
+        {
+            TestMatches("SomeFutureListQuery", DoesNotMatch);
+        }
+
+        [Fact]
+        void TestMatchesReturnsTrueIfResponseTypeIsOptionalOfProvidedType()
+        {
+            TestMatches("SomeOptionalQueryResponse", Matches);
+        }
+        
     }
 }
