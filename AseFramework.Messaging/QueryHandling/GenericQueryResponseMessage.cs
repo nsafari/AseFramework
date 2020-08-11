30db1de8fb57328321681ea03c5644d0aac84f38
nSafari <nassersafari@gmail.com>
2020-08-11 06:09:20 +0430

    Added GenericQueryResponseMessage.cs

---
+++ AseFramework.Messaging/QueryHandling/GenericQueryResponseMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using Ase.Messaging.Messaging;
+
+namespace Ase.Messaging.QueryHandling
+{
+    /// <summary>
+    /// QueryResponseMessage implementation that takes all properties as constructor parameters.
+    /// </summary>
+    /// <typeparam name="R">The type of return value contained in this response</typeparam>
+    public class GenericQueryResponseMessage<R> : GenericResultMessage<R>, IQueryResponseMessage<R>
+        where R : class
+    {
+        public GenericQueryResponseMessage(R commandResult) : base(commandResult)
+        {
+        }
+
+        public GenericQueryResponseMessage(Exception exception) : base(exception)
+        {
+        }
+
+        public GenericQueryResponseMessage(R commandResult, IImmutableDictionary<string, object> metaData) : base(
+            commandResult, metaData)
+        {
+        }
+
+        public GenericQueryResponseMessage(Exception exception, IImmutableDictionary<string, object> metaData) : base(
+            exception, metaData)
+        {
+        }
+
+        public GenericQueryResponseMessage(IMessage<R> @delegate) : base(@delegate)
+        {
+        }
+
+        public GenericQueryResponseMessage(IMessage<R> @delegate, Exception exception) : base(@delegate, exception)
+        {
+        }
+
+        public IQueryResponseMessage<R> WithMetaData(ReadOnlyDictionary<string, object> metaData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryResponseMessage<R> AndMetaData(ReadOnlyDictionary<string, object> additionalMetaData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
