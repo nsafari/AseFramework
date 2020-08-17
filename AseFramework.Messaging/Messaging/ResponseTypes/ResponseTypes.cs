6c9b3080161651c4c831027277db511f4e3416cb
nSafari <nassersafari@gmail.com>
2020-08-17 07:03:21 +0430

    Adding ResponseTypes

---
+++ AseFramework.Messaging/Messaging/ResponseTypes/ResponseTypes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ase.Messaging.Messaging.ResponseTypes
+{
+    /// <summary>
+    /// Utility class containing static methods to obtain instances of {@link ResponseType}.
+    /// </summary>
+    public static class ResponseTypes
+    {
+        /// <summary>
+        /// Specify the desire to retrieve a single instance of type {@code R} when performing a query.
+        /// </summary>
+        /// <param name="type">the {@code R} which is expected to be the response type</param>
+        /// <typeparam name="R">the generic type of the instantiated {@link ResponseType}</typeparam>
+        /// <returns>a {@link ResponseType} specifying the desire to retrieve a single instance of type {@code R}</returns>
+        public static IResponseType<R> InstanceOf<R>(Type type)
+            where R : class
+        {
+            return new InstanceResponseType<R>(type);
+        }
+
+
+        /// <summary>
+        /// Specify the desire to retrieve a collection of instances of type {@code R} when performing a query.
+        /// </summary>
+        /// <param name="type">the {@code R} which is expected to be the response type</param>
+        /// <typeparam name="R">the generic type of the instantiated {@link ResponseType}</typeparam>
+        /// <returns>a {@link ResponseType} specifying the desire to retrieve a collection of instances of type {@code R}</returns>
+        public static IResponseType<List<R>> MultipleInstancesOf<R>(Type type)
+            where R : class
+        {
+            return new MultipleInstancesResponseType<R>(type);
+        }
+    }
+}
