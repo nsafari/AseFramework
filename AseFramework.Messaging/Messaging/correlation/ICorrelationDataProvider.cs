cfa65d7c3352fa70ffa92d0d4adcf388394e6b14
nSafari <nassersafari@gmail.com>
2020-08-22 02:33:22 +0430

    Adding ICorrelationDataProvider

---
+++ AseFramework.Messaging/Messaging/correlation/ICorrelationDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Ase.Messaging.Messaging.correlation
+{
+    /// <summary>
+    /// Object defining the data from a Message that should be attached as correlation data to messages generated as
+    /// result of the processing of that message.
+    /// </summary>
+    public interface ICorrelationDataProvider
+    {
+        delegate IDictionary<string, R> CorrelationDataFor<R>(IMessage<R> message)
+            where R : class;
+    }
+}
