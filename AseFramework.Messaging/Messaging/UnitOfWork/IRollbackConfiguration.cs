87295551eff1e0338ef16789eab7fd912ee07834
nSafari <nassersafari@gmail.com>
2020-08-23 07:27:54 +0430

    Adding IRollbackConfiguration

---
+++ AseFramework.Messaging/Messaging/UnitOfWork/IRollbackConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ase.Messaging.Messaging.UnitOfWork
+{
+    /// <summary>
+    /// The RollbackConfiguration defines if a Unit of Work should be rolled back when an exception is raised during the
+    /// processing of a Message.
+    /// <p/>
+    /// Note that the Unit of Work will always throw any exceptions raised during processing, regardless of whether or not
+    /// the Unit of Work is rolled back.
+    /// </summary>
+    public interface IRollbackConfiguration
+    {
+        /// <summary>
+        /// Decides whether the given {@code throwable} should trigger a rollback.
+        /// </summary>
+        /// <param name="throwable">the Throwable to evaluate</param>
+        /// <returns>{@code true} if the UnitOfWork should be rolled back, {@code false} otherwise</returns>
+        bool rollBackOn(Exception throwable);
+    }
+}
---
+++ AseFramework.Messaging/Messaging/UnitOfWork/RollbackConfigurationType.cs
@@ -0,0 +1,22 @@
+﻿namespace Ase.Messaging.Messaging.UnitOfWork
+{
+    public enum RollbackConfigurationType
+    {
+        /// <summary>
+        /// Configuration that never performs a rollback of the unit of work.
+        /// </summary>
+        NEVER,
+        /// <summary>
+        /// Configuration that prescribes a rollback on any sort of exception or error.
+        /// </summary>
+        ANY_THROWABLE,
+        /// <summary>
+        /// Configuration that prescribes a rollback on any sort of unchecked exception, including errors.
+        /// </summary>
+        UNCHECKED_EXCEPTIONS,
+        /// <summary>
+        /// Configuration that prescribes a rollback on runtime exceptions only.
+        /// </summary>
+        RUNTIME_EXCEPTIONS
+    }
+}
