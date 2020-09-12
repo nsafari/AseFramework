ceb0e82a53adfad46e731a09eeac6699ca32f6c5
nSafari <nassersafari@gmail.com>
2020-09-12 15:38:53 +0430

    Adding GenericSubscriptionQueryMessage

---
+++ AseFramework.Messaging/QueryHandling/GenericSubscriptionQueryMessage.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Immutable;
+using Ase.Messaging.Messaging;
+using Ase.Messaging.Messaging.ResponseTypes;
+
+namespace Ase.Messaging.QueryHandling
+{
+    /// <summary>
+    /// Generic implementation of the {@link SubscriptionQueryMessage}. Unless explicitly provided, it assumes the {@code
+    /// queryName} of the message is the fully qualified class name of the message's payload.
+    /// </summary>
+    /// <typeparam name="Q">The type of payload expressing the query in this message</typeparam>
+    /// <typeparam name="I">The type of initial response expected from this query</typeparam>
+    /// <typeparam name="U">The type of incremental updates expected from this query</typeparam>
+    public class GenericSubscriptionQueryMessage<Q, I, U> : GenericQueryMessage<Q, I>, ISubscriptionQueryMessage<Q, I, U>
+        where Q : class where I : class where U : class
+    {
+        private readonly IResponseType<U> _updateResponseType;
+
+        /// <summary>
+        /// Initializes the message with the given {@code payload}, expected {@code responseType} and expected {@code
+        /// updateResponseType}. The query name is set to the fully qualified class name of the {@code payload}.
+        /// </summary>
+        /// <param name="payload">The payload expressing the query</param>
+        /// <param name="responseType">The expected response type</param>
+        /// <param name="updateResponseType">The expected type of incremental updates</param>
+        public GenericSubscriptionQueryMessage(
+            Q payload,
+            IResponseType<I> responseType,
+            IResponseType<U> updateResponseType
+        ) : this(payload, payload.GetType().ToString(), responseType, updateResponseType)
+        {
+        }
+
+        /// <summary>
+        /// Initializes the message with the given {@code payload}, {@code queryName}, expected {@code responseType} and
+        /// expected {@code updateResponseType}.
+        /// </summary>
+        /// <param name="payload">The payload expressing the query</param>
+        /// <param name="queryName">The name identifying the query to execute</param>
+        /// <param name="responseType">The expected response type</param>
+        /// <param name="updateResponseType">The expected type of incremental updates</param>
+        public GenericSubscriptionQueryMessage(
+            Q payload,
+            string queryName,
+            IResponseType<I> responseType,
+            IResponseType<U> updateResponseType
+        ) : base(payload, queryName, responseType)
+        {
+            _updateResponseType = updateResponseType;
+        }
+
+        /// <summary>
+        /// Initializes the message, using given {@code delegate} as the carrier of payload and metadata and given {@code
+        /// queryName}, expected {@code responseType} and expected {@code updateResponseType}.
+        /// </summary>
+        /// <param name="delegate">The message containing the payload and meta data for this message</param>
+        /// <param name="queryName">The name identifying the query to execute</param>
+        /// <param name="responseType">The expected response type</param>
+        /// <param name="updateResponseType">The expected type of incremental updates</param>
+        public GenericSubscriptionQueryMessage(
+            IMessage<Q> @delegate,
+            string queryName,
+            IResponseType<I> responseType,
+            IResponseType<U> updateResponseType
+        ) : base(@delegate, queryName, responseType)
+        {
+            _updateResponseType = updateResponseType;
+        }
+
+        public IResponseType<U> GetUpdateResponseType()
+        {
+            return _updateResponseType;
+        }
+
+        public new GenericSubscriptionQueryMessage<Q, I, U> WithMetaData(IImmutableDictionary<string, object> metaData)
+        {
+            return new GenericSubscriptionQueryMessage<Q, I, U>(
+                Delegate().WithMetaData(metaData),
+                GetQueryName(),
+                GetResponseType(),
+                _updateResponseType
+            );
+        }
+
+        public new GenericSubscriptionQueryMessage<Q, I, U> AndMetaData(IImmutableDictionary<string, object> metaData)
+        {
+            return new GenericSubscriptionQueryMessage<Q, I, U>(
+                Delegate().AndMetaData(metaData),
+                GetQueryName(),
+                GetResponseType(),
+                _updateResponseType);
+        }
+    }
+}
---
+++ AseFramework.Messaging/QueryHandling/ISubscriptionQueryMessage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Immutable;
+using Ase.Messaging.Messaging.ResponseTypes;
+
+namespace Ase.Messaging.QueryHandling
+{
+    /// <summary>
+    /// Message type that carries a Subscription Query: a request for information. Besides a payload, Subscription Query
+    /// Messages also carry the expected response type and update type. The response type is the type of result expected by
+    /// the caller. The update type is type of incremental updates.
+    /// <p>
+    /// Handlers should only answer a query if they can respond with the appropriate response type and update type.
+    /// </summary>
+    /// <typeparam name="Q">the type of payload</typeparam>
+    /// <typeparam name="I">the type of initial response</typeparam>
+    /// <typeparam name="U">the type of incremental responses</typeparam>
+    public interface ISubscriptionQueryMessage<Q, I, U> : IQueryMessage<Q, I>
+        where Q : class where I : class where U : class
+    {
+        /// <summary>
+        /// Returns the type of incremental responses.
+        /// </summary>
+        /// <returns>the type of incremental responses</returns>
+        IResponseType<U> GetUpdateResponseType();
+
+        /// <summary>
+        /// Returns a copy of this SubscriptionQueryMessage with the given {@code metaData}. The payload remains unchanged.
+        /// </summary>
+        /// <param name="metaData">The new MetaData for the SubscriptionQueryMessage</param>
+        /// <returns>a copy of this message with the given MetaData</returns>
+        new ISubscriptionQueryMessage<Q, I, U> WithMetaData(IImmutableDictionary<string, object> metaData);
+
+        /// <summary>
+        /// Returns a copy of this SubscriptionQueryMessage with its MetaData merged with given {@code metaData}. The payload
+        /// remains unchanged.
+        /// </summary>
+        /// <param name="additionalMetaData">The MetaData to merge into the SubscriptionQueryMessage</param>
+        /// <returns>a copy of this message with the given additional MetaData</returns>
+        new ISubscriptionQueryMessage<Q, I, U> AndMetaData(IImmutableDictionary<string, object> additionalMetaData);
+    }
+}
