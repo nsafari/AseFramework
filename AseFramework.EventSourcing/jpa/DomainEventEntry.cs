e8dec503fbf00a6c2887b32b3864aee20ff2bb03
nSafari <nassersafari@gmail.com>
2020-09-21 07:19:31 +0330

    Updating DomainEventEntry

--- AseFramework.EventSourcing/jpa/DomainEventEntry.cs
+++ AseFramework.EventSourcing/jpa/DomainEventEntry.cs
@@ -1,10 +1,31 @@
-﻿using NHMA = NHibernate.Mapping.Attributes;
+﻿using Ase.Messaging.EventHandling;
+using Ase.Messaging.Serialization;
+using NHMA = NHibernate.Mapping.Attributes;
 
 namespace AseFramework.EventSourcing.jpa
 {
-
-    public class DomainEventEntry
+    /// <summary>
+    /// Default implementation of a tracked domain event entry. This implementation is used by the {@link
+    /// JpaEventStorageEngine} to store events. Event payload and metadata are serialized to a byte array.
+    /// </summary>
+    public class DomainEventEntry : AbstractSequencedDomainEventEntry<byte[]>
     {
         
+        /// <summary>
+        /// Construct a new default domain event entry from a published domain event message to enable storing the event or
+        /// sending it to a remote location. The event payload and metadata will be serialized to a byte array.
+        /// <p>
+        /// The given {@code serializer} will be used to serialize the payload and metadata in the given {@code eventMessage}.
+        /// The type of the serialized data will be the same as the given {@code contentType}.
+        /// </summary>
+        /// <param name="eventMessage">The event message to convert to a serialized event entry</param>
+        /// <param name="serializer">The serializer to convert the event</param>
+        public DomainEventEntry(IDomainEventMessage<byte[]> eventMessage, ISerializer serializer) 
+            : base(eventMessage, serializer, typeof(byte[]))
+        {
+        }
+        
+        protected DomainEventEntry() {
+        }
     }
 }
