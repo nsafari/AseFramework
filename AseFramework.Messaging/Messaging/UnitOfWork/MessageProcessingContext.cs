032500bb9fb62dc8322f781f3fe02565f40abaf8
nSafari <nassersafari@gmail.com>
2020-09-30 11:04:38 +0330

    Adding MessageProcessingContext

---
+++ AseFramework.Messaging/Messaging/UnitOfWork/MessageProcessingContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Nito.Collections;
+
+namespace Ase.Messaging.Messaging.UnitOfWork
+{
+    public class MessageProcessingContext<T>
+        where T : IMessage<object>
+    {
+
+        private static readonly Deque<T> Empty = new Deque<T>();
+
+        private readonly IDictionary<Phase, Deque<Action<IUnitOfWork<T>>>> handlers = new IDi<>(Phase.class);
+        private T message;
+        private ExecutionResult executionResult;
+
+    }
+}
