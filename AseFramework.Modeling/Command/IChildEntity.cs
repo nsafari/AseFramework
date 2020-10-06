887759ce6bb8a00ab9101ec2994b0d0695051a25
nSafari <nassersafari@gmail.com>
2020-10-06 06:24:05 +0330

    Adding IChildEntity

---
+++ AseFramework.Modeling/Command/IChildEntity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Ase.Messaging.Annotation;
+using Ase.Messaging.EventHandling;
+
+namespace AseFramework.Modeling.Command
+{
+    /// <summary>
+    /// Interface describing en entity that is a child of another entity.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IChildEntity<T>
+    {
+        /// <summary>
+        /// Publish the given {@code msg} to the appropriate handlers on the given {@code declaringInstance}
+        /// </summary>
+        /// <param name="msg">The message to publish</param>
+        /// <param name="declaringInstance">The instance of this entity to invoke handlers on</param>
+        void Publish(IEventMessage<object> msg, T declaringInstance);
+        
+        /// <summary>
+        /// Returns the command handlers declared in this entity
+        /// </summary>
+        /// <typeparam name="TR"></typeparam>
+        /// <returns>a list of message handling members that are capable of processing command messages</returns>
+        IList<IMessageHandlingMember<TR>> CommandHandlers<TR>() where TR: T;
+    }
+}
