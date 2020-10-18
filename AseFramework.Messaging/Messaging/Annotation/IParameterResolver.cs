a1c99ba6c6f23be37061645610accba170423a8c
nSafari <nassersafari@gmail.com>
2020-10-18 11:49:33 +0330

    Adding IParameterResolver

---
+++ AseFramework.Messaging/Messaging/Annotation/IParameterResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ase.Messaging.Messaging.Annotation
+{
+    /// <summary>
+    /// Interface for a mechanism that resolves handler method parameter values from a given {@link Message}.
+    /// 
+    /// </summary>
+    /// <typeparam name="T">The type of parameter returned by this resolver</typeparam>
+    public interface IParameterResolver<T>
+    {
+        /// <summary>
+        /// Resolves the parameter value to use for the given {@code message}, or {@code null} if no suitable
+        /// parameter value can be resolved.
+        /// </summary>
+        /// <param name="message">The message to resolve the value from</param>
+        /// <returns>the parameter value for the handler</returns>
+        T ResolveParameterValue(IMessage<object> message);
+
+        /// <summary>
+        /// Indicates whether this resolver is capable of providing a value for the given {@code message}.
+        /// </summary>
+        /// <param name="message">The message to evaluate</param>
+        /// <returns>{@code true} if this resolver can provide a value for the message, otherwise {@code false}</returns>
+        bool Matches(IMessage<object> message);
+
+        /// <summary>
+        /// Returns the class of the payload that is supported by this resolver. Defaults to the {@link Object} class
+        /// indicating that the payload type is irrelevant for this resolver.
+        /// </summary>
+        /// <returns>The class of the payload that is supported by this resolver</returns>
+        Type SupportedPayloadType()
+        {
+            return typeof(object);
+        }
+    }
+}
