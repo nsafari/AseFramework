52274005246ad1f58859350862e845ab0500e7db
nSafari <nassersafari@gmail.com>
2020-10-22 00:56:04 +0330

    Adding IParameterResolverFactory

---
+++ AseFramework.Messaging/Messaging/Annotation/IParameterResolverFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace Ase.Messaging.Messaging.Annotation
+{
+    public interface IParameterResolverFactory
+    {
+        IParameterResolver<T> CreateInstance<T>(MethodBase executable, ParameterInfo[] parameters, int parameterIndex);
+
+    }
+}
