d9a3ce9a00a484ca68fea477fc09fc3271a9677f
nSafari <nassersafari@gmail.com>
2020-10-25 08:48:06 +0330

    Adding AnnotatedCommandHandlerInterceptor

---
+++ AseFramework.Modeling/Command/Inspection/AnnotatedCommandHandlerInterceptor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Ase.Messaging.Annotation;
+using Ase.Messaging.CommandHandling;
+using Ase.Messaging.Messaging;
+using Ase.Messaging.Messaging.UnitOfWork;
+
+namespace AseFramework.Modeling.Command.Inspection
+{
+    /// <summary>
+    /// Annotated command handler interceptor on aggregate. Will invoke the delegate to the real interceptor method.
+    /// </summary>
+    /// <typeparam name="TTarget">The type of entity to which the message handler will delegate the actual handling of the message</typeparam>
+    /// <typeparam name="TPayload">The type of payload that has been passed to the command message</typeparam>
+    public class AnnotatedCommandHandlerInterceptor<TTarget, TPayload>
+        : IMessageHandlerInterceptor<ICommandMessage<TPayload>, TPayload>
+        where TPayload : class
+    {
+        private readonly IMessageHandlingMember<TTarget> _delegate;
+        private readonly TTarget _target;
+
+        /// <summary>
+        /// Initializes annotated command handler interceptor with delegate handler and target on which handler is to be
+        /// invoked.
+        /// </summary>
+        /// <param name="delegate">delegate command handler interceptor</param>
+        /// <param name="target">on which command handler interceptor is to be invoked</param>
+        public AnnotatedCommandHandlerInterceptor(IMessageHandlingMember<TTarget> @delegate, TTarget target)
+        {
+            _delegate = @delegate;
+            _target = target;
+        }
+
+        public object Handle(IUnitOfWork<ICommandMessage<TPayload>, TPayload> unitOfWork,
+            IInterceptorChain interceptorChain)
+        {
+            InterceptorChainParameterResolverFactory.Initialize<ICommandMessage<TPayload>, TPayload>(interceptorChain);
+
+            object result = _delegate.Handle(unitOfWork.GetMessage(), _target);
+
+            if (_delegate
+                .Unwrap<ICommandHandlerInterceptorHandlingMember<TTarget>>(
+                    typeof(ICommandHandlerInterceptorHandlingMember<>)
+                )?.ShouldInvokeInterceptorChain() ?? false)
+            {
+                result = interceptorChain.Proceed();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AseFramework.Modeling/Command/Inspection/ICommandHandlerInterceptorHandlingMember.cs
@@ -0,0 +1,21 @@
+﻿using Ase.Messaging.Annotation;
+
+namespace AseFramework.Modeling.Command.Inspection
+{
+    /// <summary>
+    /// Interface specifying a message handler capable of intercepting a command.
+    /// </summary>
+    /// <typeparam name="T">the type of entity to which the message handler will delegate tha actual interception</typeparam>
+    public interface ICommandHandlerInterceptorHandlingMember<T> : IMessageHandlingMember<T>
+    {
+        
+        /// <summary>
+        /// Indicates whether interceptor chain (containing a command handler) should be invoked automatically or command
+        /// handler interceptor will invoke it manually.
+        /// </summary>
+        /// <returns>{@code true} if interceptor chain should be invoked automatically</returns>
+        bool ShouldInvokeInterceptorChain();
+
+        
+    }
+}
