b01bcd3bd35bb43a4dac1a7c7350b3bbfcfc84fe
nSafari <nassersafari@gmail.com>
2020-10-31 22:05:38 +0330

    Adding DefaultInterceptorChain

---
+++ AseFramework.Messaging/Messaging/DefaultInterceptorChain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Ase.Messaging.Messaging.UnitOfWork;
+
+namespace Ase.Messaging.Messaging
+{
+    public class DefaultInterceptorChain<TMessage, TPayload> : IInterceptorChain
+        where TMessage : IMessage<TPayload> where TPayload : class
+    {
+        private readonly IMessageHandler<TMessage, TPayload> _handler;
+        private readonly IEnumerator<IMessageHandlerInterceptor<TMessage, TPayload>> _chain;
+        private readonly IUnitOfWork<TMessage, TPayload> _unitOfWork;
+
+        public DefaultInterceptorChain(IUnitOfWork<TMessage, TPayload> unitOfWork,
+            IEnumerator<IMessageHandlerInterceptor<TMessage, TPayload>> interceptors,
+            IMessageHandler<TMessage, TPayload> handler)
+        {
+            _handler = handler;
+            _chain = interceptors;
+            _unitOfWork = unitOfWork;
+        }
+
+        public object Proceed()
+        {
+        }
+    }
+}
