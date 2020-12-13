189456e126ed61f47e3dd4b29da176d1da98eb7b
nSafari <nassersafari@gmail.com>
2020-12-13 18:32:47 +0330

    Adding IWrappedToken.cs

---
+++ AseFramework.Messaging/EventHandling/IWrappedToken.cs
@@ -0,0 +1,35 @@
+﻿namespace Ase.Messaging.EventHandling
+{
+    public interface IWrappedToken : ITrackingToken
+    {
+        static ITrackingToken UnwrapLowerBound(ITrackingToken token)
+        {
+            return token is IWrappedToken wrappedToken ? wrappedToken.LowerBound() : token;
+        }
+
+        static ITrackingToken UnwrapUpperBound(ITrackingToken token)
+        {
+            return token is IWrappedToken wrappedToken ? ((IWrappedToken) token).UpperBound() : token;
+        }
+
+        static R? Unwrap<R>(ITrackingToken token)
+            where R : class, ITrackingToken
+        {
+            return token switch
+            {
+                IWrappedToken wrappedToken => wrappedToken.Unwrap<R>(),
+                R trackingToken => trackingToken,
+                _ => null
+            };
+        }
+
+        ITrackingToken AdvancedTo(ITrackingToken newToken);
+
+        ITrackingToken LowerBound();
+
+        ITrackingToken UpperBound();
+
+        R? Unwrap<R>()
+            where R : class, ITrackingToken;
+    }
+}
