79ac845d272e4339f80f4cee1a4842a45eb7806b
nSafari <nassersafari@gmail.com>
2020-12-17 07:17:04 +0330

    Using ImmutableSortedSet

--- AseFramework.Messaging/EventHandling/GapAwareTrackingToken.cs
+++ AseFramework.Messaging/EventHandling/GapAwareTrackingToken.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Ase.Messaging.Common;
 using NHibernate.Engine;
 
@@ -9,7 +12,7 @@ namespace Ase.Messaging.EventHandling
     public class GapAwareTrackingToken : ITrackingToken
     {
         private readonly long _index;
-        private readonly SortedSet<long> _gaps;
+        private readonly ImmutableSortedSet<long> _gaps;
         private readonly long _gapTruncationIndex;
 
         public static GapAwareTrackingToken NewInstance(long index, Collection<long> gaps)
@@ -17,23 +20,23 @@ namespace Ase.Messaging.EventHandling
             return new GapAwareTrackingToken(index, gaps);
         }
 
-        public GapAwareTrackingToken(long index, Collection<long> gaps) : this(index, createSortedSetOf(gaps, index), 0)
+        public GapAwareTrackingToken(long index, Collection<long> gaps) : this(index, CreateSortedSetOf(gaps, index), 0)
         {
         }
         
-        private GapAwareTrackingToken(long index, SortedSet<long> gaps, long gapTruncationIndex) {
+        private GapAwareTrackingToken(long index, ImmutableSortedSet<long> gaps, long gapTruncationIndex) {
             _index = index;
             _gaps = gaps;
             _gapTruncationIndex = gapTruncationIndex;
         }
         
-        protected static SortedSet<long> CreateSortedSetOf(Collection<long> gaps, long index) {
+        protected static ImmutableSortedSet<long> CreateSortedSetOf(Collection<long> gaps, long index) {
             if (gaps.Count == 0) {
-                return new SortedSet<long>();
+                return ImmutableSortedSet.Create<long>();
             }
-            SortedList gapSet = SortedList.Synchronized(new SortedList(gaps));
-            Assert.isTrue(gapSet.last() < index,
-                () -> String.format("Gap indices [%s] should all be smaller than head index [%d]", gaps, index));
+            ImmutableSortedSet<long> gapSet = ImmutableSortedSet.CreateRange(gaps.AsEnumerable());
+            Assert.IsTrue(gapSet.Max < index, () => throw new ArgumentException(
+                $"Gap indices {gaps} should all be smaller than head index {index}"));
             return gapSet;
         }
 
