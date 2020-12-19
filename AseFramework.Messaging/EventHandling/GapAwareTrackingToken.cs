3b2201f441e300a1c6651eddb012abc7f8849537
nSafari <nassersafari@gmail.com>
2020-12-19 04:39:30 +0100

    Adding AdvanceTo

--- AseFramework.Messaging/EventHandling/GapAwareTrackingToken.cs
+++ AseFramework.Messaging/EventHandling/GapAwareTrackingToken.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
@@ -12,7 +13,7 @@ namespace Ase.Messaging.EventHandling
     public class GapAwareTrackingToken : ITrackingToken
     {
         private readonly long _index;
-        private readonly ImmutableSortedSet<long> _gaps;
+        private ImmutableSortedSet<long> _gaps;
         private readonly long _gapTruncationIndex;
 
         public static GapAwareTrackingToken NewInstance(long index, Collection<long> gaps)
@@ -23,22 +24,54 @@ namespace Ase.Messaging.EventHandling
         public GapAwareTrackingToken(long index, Collection<long> gaps) : this(index, CreateSortedSetOf(gaps, index), 0)
         {
         }
-        
-        private GapAwareTrackingToken(long index, ImmutableSortedSet<long> gaps, long gapTruncationIndex) {
+
+        private GapAwareTrackingToken(long index, ImmutableSortedSet<long> gaps, long gapTruncationIndex)
+        {
             _index = index;
             _gaps = gaps;
             _gapTruncationIndex = gapTruncationIndex;
         }
-        
-        protected static ImmutableSortedSet<long> CreateSortedSetOf(Collection<long> gaps, long index) {
-            if (gaps.Count == 0) {
+
+        protected static ImmutableSortedSet<long> CreateSortedSetOf(Collection<long> gaps, long index)
+        {
+            if (gaps.Count == 0)
+            {
                 return ImmutableSortedSet.Create<long>();
             }
+
             ImmutableSortedSet<long> gapSet = ImmutableSortedSet.CreateRange(gaps.AsEnumerable());
             Assert.IsTrue(gapSet.Max < index, () => throw new ArgumentException(
                 $"Gap indices {gaps} should all be smaller than head index {index}"));
             return gapSet;
         }
 
+        public GapAwareTrackingToken AdvanceTo(long index, int maxGapOffset)
+        {
+            long newIndex;
+            ConcurrentBag<long> gaps = new ConcurrentBag<long>(_gaps);
+            if (_gaps.Contains(index))
+            {
+                _gaps = _gaps.Remove(index);
+                newIndex = _index;
+            }
+            else if (index > _index)
+            {
+                newIndex = index;
+                foreach (var i in Enumerable.Range((int) (_index + 1L), (int)index))
+                {
+                    _gaps = _gaps.Add(i);
+                }
+            }
+            else
+            {
+                throw new ArgumentException(
+                    $"The given index {index} should be larger than the token index {_index} or " +
+                    $"be one of the token's gaps {gaps}");
+            }
+
+            long smalledAllowedGap = Math.Max(_gapTruncationIndex, newIndex - maxGapOffset);
+            gaps.removeAll(gaps.headSet(smalledAllowedGap));
+            return new GapAwareTrackingToken(newIndex, gaps, smalledAllowedGap);
+        }
     }
 }
