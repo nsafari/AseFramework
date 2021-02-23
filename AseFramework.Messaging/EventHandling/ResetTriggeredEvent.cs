6610c28e9fbf760d5ecc2932e651d75fcc58b3ab
nSafari <nassersafari@gmail.com>
2021-02-23 04:05:54 +0100

    Adding ResetTriggeredEvent

---
+++ AseFramework.Messaging/EventHandling/ResetTriggeredEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace Ase.Messaging.EventHandling
+{
+    /// <summary>
+    /// Event Payload used to trigger reset handlers. This class doesn't contain any information and is merely used to
+    /// trigger a suitable handler method.
+    /// 
+    /// @see ResetHandler @ResetHandler
+    /// </summary>
+    public class ResetTriggeredEvent
+    {
+        
+    }
+}
