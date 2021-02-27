1f201d709ffe0c0f320168b80cb4c5605c65cdfd
nSafari <nassersafari@gmail.com>
2021-02-27 04:00:20 +0100

    Adding comments

--- AseFramework.Messaging/EventHandling/ResetNotSupportedException.cs
+++ AseFramework.Messaging/EventHandling/ResetNotSupportedException.cs
@@ -8,10 +8,19 @@ namespace Ase.Messaging.EventHandling
     /// </summary>
     public class ResetNotSupportedException: AseNonTransientException
     {
+        /// <summary>
+        /// saveBatchMeLoConsumptionDataTest
+        /// </summary>
+        /// <param name="message">a message describing the cause of the exception</param>
         public ResetNotSupportedException(string message) : base(message)
         {
         }
 
+        /// <summary>
+        /// Initialize the exception with given {@code message} and {@code cause}.
+        /// </summary>
+        /// <param name="message">a message describing the cause of the exception</param>
+        /// <param name="innerException">the cause of this exception</param>
         public ResetNotSupportedException(string message, Exception? innerException) : base(message, innerException)
         {
         }
